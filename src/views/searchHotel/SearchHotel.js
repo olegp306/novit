5b348d276e671eafe24515d0147e911827cacca2
olegp306 <olegp306@gmail.com>
2023-02-05 17:48:14 +0100

    rename ids

--- src/views/searchHotel/SearchHotel.js
+++ src/views/searchHotel/SearchHotel.js
@@ -14,74 +14,77 @@ export default function SearchHotel({
     hotelCategoryOptions }) {
     return (
         <Form>
-            <div class="row">
-                <div class="col">
-                    <Select
-                        name="departureCity"
-                        label="Lähtepunkt"
-                        // options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
-                        options={despatureCityOptions}
-                    />
+            <div class="form">
+                <div class="row">
+                    <div class="col">
+                        <Select
+                            name="departureCity"
+                            label="Lähtepunkt"
+                            // options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
+                            options={despatureCityOptions}
+                        />
+                    </div>
+                    <div class="col-6">
+                        <Select
+                            name="country"
+                            label="Sihtriik"
+                            // options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
+                            options={countryProposalOptions}
+                        />
+                    </div>
+                    <div class="col">
+                        <Select
+                            name="departureCity"
+                            label="Linn"
+                            options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
+                        />
+                    </div>
                 </div>
-                <div class="col-6">
-                    <Select
-                        name="countryProposal"
-                        label="Sihtriik"
-                        // options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
-                        options={countryProposalOptions}
-                    />
-                </div>
-                <div class="col">
-                    <Select
-                        name="departureCity"
-                        label="Linn"
-                        options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
-                    />
-                </div>
+
+                <Input
+                    name="calendar"
+                    label="Kuupäev"
+                    options={calendarOptionsOptions}
+                />
+
+                <Checkbox
+                    name="isFlex"
+                    label=" +/- 7 päeva"
+                />
+
+                <Range
+                    name="daysQuantity"
+                    label="Ööde arv:">
+                </Range>
+
+                <Select
+                    name="hotelCategory"
+                    label="Hotelli kategooria"
+                    options={hotelCategoryOptions}
+                />
+
+                <Select
+                    name="food"
+                    label="Toitlustus:"
+                    options={foodOptions}
+                />
+                <Range
+                    name="pricePerAdult"
+                    label="Hind 1 täiskasvanu kohta:"
+                >
+                </Range>
+
+                <Select
+                    name="searchHotelName"
+                    label="Hotelli nimi:"
+                />
+
+
+                <Button variant="primary" type="submit">
+                    OTSI REISI
+                </Button>
+
             </div>
-
-            <Input
-                name="calendar"
-                label="Kuupäev"
-                options={calendarOptionsOptions}
-            />
-
-            <Checkbox
-                name="isFlex"
-                label=" +/- 7 päeva"
-            />
-
-            <Range
-                name="daysQuantity"
-                label="Ööde arv:">
-            </Range>
-
-            <Select
-                name="hotelCategory"
-                label="Hotelli kategooria"
-                options={hotelCategoryOptions}
-            />
-
-            <Select
-                name="food"
-                label="Toitlustus:"
-                options={foodOptions}
-            />
-            <Range
-                name="pricePerAdult"
-                label="Hind 1 täiskasvanu kohta:"
-            >
-            </Range>
-
-            <Select
-                name="searchHotelName"
-                label="Hotelli nimi:"
-            />
-
-
-            <Button variant="primary" type="submit">
-                OTSI REISI
-            </Button>
         </Form>
     )
 }
