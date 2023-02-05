bed371f5eadcb00a11fa9e743d29546b59770a04
olegp306 <olegp306@gmail.com>
2023-02-05 14:11:21 +0100

    stable form draft

--- src/views/searchHotel/SearchHotel.js
+++ src/views/searchHotel/SearchHotel.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Checkbox from '../../components/checkbox/Checkbox';
 import Input from '../../components/input/Input';
 import Select from '../../components/select';
+import Range from '../../components/range';
 
 export default function SearchHotel() {
     return (
@@ -28,11 +29,52 @@ export default function SearchHotel() {
                 name="calendar"
                 label="Kuupäev"
                 options={[{ value: 1, label: "one" }, { value: 2, label: "two" }]}
-           />
+            />
 
             <Checkbox
                 name="isFlex"
                 label=" +/- 7 päeva"
+            />
+            <Range
+                name="daysQuantity"
+                label="Ööde arv:">
+            </Range>
+
+            <Select
+                name="hotelCategory"
+                label="Hotelli kategooria"
+                options={[
+                    { value: 1, label: "1 tärn" },
+                    { value: 2, label: "2 tärni" },
+                    { value: 3, label: "3 tärni" },
+                    { value: 4, label: "4 tärni" },
+                    { value: 5, label: "5 tärni" },
+                    { value: 6, label: "--Kõik--" },
+                ]}
+            />
+
+            <Select
+                name="food"
+                label="Toitlustus:"
+                options={[
+                    { value: "AI", label: "Kõik hinnas" },
+                    { value: "BB", label: "Hommikusöögiga" },
+                    { value: "FB", label: "Hommiku-, lõuna ja õhtusöögiga" },
+                    { value: "HB", label: "Hommiku- ja õhtusöögiga" },
+                    { value: "RO", label: "Söökideta" },
+                    { value: "UAI", label: "Kõik hinnas +" },
+
+                ]}
+            />
+            <Range
+                name="pricePerAdult"
+                label="Hind 1 täiskasvanu kohta:"              
+                >
+            </Range>
+
+            <Select
+                name="searchHotelName"
+                label="Hotelli nimi:"                
             />
             
 
