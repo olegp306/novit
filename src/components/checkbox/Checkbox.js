a4db359e05d565f570751fcf7f2bfb3d71fc0c64
olegp306 <olegp306@gmail.com>
2023-02-05 14:39:03 +0100

    fix checkbox

--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-const Checkbox = ({ name, label, value, className, onChange, ...rest }) => {
+const Checkbox = ({ name, label, value, ...rest }) => {
   return (
     <Form.Group className="mb-3">
       <Form.Label>{label}</Form.Label>
       <Form.Check
         name={name}
+        {...rest}
       />
     </Form.Group>
   )
