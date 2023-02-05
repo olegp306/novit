a228c0fadb499f517d04b2d8ed38e0577c399dd7
olegp306 <olegp306@gmail.com>
2023-02-05 17:48:45 +0100

    add container logic, add form state

--- src/views/searchHotel/SearchHotelContainer.js
+++ src/views/searchHotel/SearchHotelContainer.js
@@ -1,18 +1,43 @@
 import React from 'react'
+import { DefaultCity, DefaultCountry } from './constans'
 import SearchHotel from './SearchHotel'
 
 export default function SearchHotelContainer() {
-
     const [despatureCityOptions, setDespatureCityOptions] = useState(null)
     const [countryProposalOptions, setCountryProposalOptions] = useState(null)
-    const [calendarOptionsOptions, setCalendarOptions] = useState(null)
+    const [calendarOptions, setCalendarOptions] = useState(null)
+
+    const [depatureCity, setDepatureCity] = useState(DefaultCity)
+    const [arrivalCity, setArrivalCity] = useState("")
+    const [country, setCountry] = useState(DefaultCountry)
+
+
+    useEffect(() => {
+        const fetchCalendarAsync = async () =>
+            await fetchCalendar({
+                dcity: depatureCity,
+                country: country
+            })
+        const data =  fetchCalendarAsync();
+        setCalendarOptions=data;
+
+
+
+        return () => {
+            second
+        }
+    }, [country, depatureCity])
+
+
 
 
     return (
         <SearchHotel
             despatureCityOptions={despatureCityOptions}
+            despatureCity={depatureCity}
             countryProposalOptions={countryProposalOptions}
-            calendarOptionsOptions={calendarOptionsOptions}
+            country={country}
+            calendarOptions={calendarOptions}
             foodOptions={foodOptions}
             hotelCategoryOptions={hotelCategoryOptions}
         />
