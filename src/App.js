d03115154bfce6e85a2be2d13516bade6e66d7a7
olegp306 <olegp306@gmail.com>
2023-02-11 09:47:28 +0100

    add logo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import SearchHotelContainer from './views/searchHotel/SearchHotelContainer';
 function App() {
   return (
     <Container className="p-3">
+      <a href="https://novit.ee/" class="mb-3" rel="home" aria-current="page">
+        <img width="250" height="83" src="https://novit.ee/wp-content/uploads/2022/09/cropped-logo2018.png" class="custom-logo" alt="Nov IT" />
+      </a>
       <Container className="p-5 mb-4 bg-light rounded-3">
+
         <div className="container">
-          <h1>Novit.ee</h1>
+          {/* <h1>Novit.ee</h1> */}
+
           <SearchHotelContainer />
         </div>
       </Container>
