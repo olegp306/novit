fa4555b3dc7267505e41803d8f87f966a7488e17
olegp306 <olegp306@gmail.com>
2023-02-13 10:04:13 +0100

    style

--- src/components/filterMark/FilterMark.js
+++ src/components/filterMark/FilterMark.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FilterMark({ label, value, ...rest }) {
     return (
         <span
-            style={{ padding: "5px 10px", borderRadius: "15px", backgroundColor: "darkgray", color: "white" }}
+            style={{ margin: "5px", padding: "5px 10px", borderRadius: "15px", backgroundColor: "darkgray", color: "white" }}
             {...rest}>
             {value}
         </span>
