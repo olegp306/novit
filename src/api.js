0338dbaae42b065bbc8a9a3360899ce19b2b374d
olegp306 <olegp306@gmail.com>
2023-02-18 13:19:19 +0100

    hf

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import testResponce from "./calendarResponce";
 const fetchDestinationCountriesUrl = `https://novit.ee/api/destinationCountry.php?option=allin`
 const fetchCalendarUrl = `https://novit.ee/api/calendarFilter.php?option=allin`
 const fetchCitiesListUrl = `https://novit.ee/api/destinationCity.php?option=allin`
-const fetchOffersUrl = 'https://novit.ee/api/searchOffers.php?option=allin '
+const fetchOffersUrl = 'https://novit.ee/api/searchOffers.php?option=allin'
 const fetchHotelsUrl = `https://novit.ee/api/hotelAutofill.php?option=allin`
 
 const fetchDataAsync = async ({ url, params }) => {
