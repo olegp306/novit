5b608cfe033488331ce2d2152711eae68d46bb85
olegp306 <olegp306@gmail.com>
2023-02-18 09:03:47 +0100

    clean up

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,17 +1,11 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { Form } from 'react-bootstrap';
 
-function valuetext(value) {
-    return `${value}°C`;
-}
 
 export default function RangeSlider({ label, value, onChange, ...rest }) {
-    // const [value, setValue] = React.useState(value);
 
     const handleChange = (event, newValue) => {
-        // setValue(newValue);
         onChange(newValue)
     };
 
@@ -20,7 +14,6 @@ export default function RangeSlider({ label, value, onChange, ...rest }) {
             <Form.Label className="mb-1">{label} <span style={{ fontWeight: '600' }}>from: {value[0]} to: {value[1]} </span></Form.Label>
             <div styles={{ width: '100%' }}>
                 <Slider
-                    // getAriaLabel={() => 'Temperature range'}
                     value={value}
                     onChange={handleChange}
                     valueLabelDisplay="auto"
