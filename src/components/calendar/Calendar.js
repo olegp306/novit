d81cacd32c999bda3205eeeea809b394a1131fee
olegp306 <olegp306@gmail.com>
2023-03-01 10:56:25 +0100

    style and label for calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,19 +3,21 @@ import { FloatingLabel, Form } from 'react-bootstrap';
 import ReactDatePicker from 'react-datepicker';
 
 
-export default function Calendar({ highlightDates, value, label, onChange, rest }) {
+export default function Calendar({ highlightDates, value, label, onChange, ...rest }) {
     return (
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" style={{ width: "auto" }}>
+            <Form.Label>{label}</Form.Label>
             <ReactDatePicker
                 id="calendar"
                 openToDate={highlightDates?.sort((date1, date2) => date1 - date2)[0]}
                 selected={value || null}
                 onChange={onChange}
                 highlightDates={highlightDates || []}
-                inline
-                placeholderText={label}
+                // inline
+                // placeholderText={label}
                 label={label}
                 className="form-control"
+
                 {...rest}
             />
 
