3e77eb178229bdce67af5d7fd1dca6348b989d08
olegp306 <olegp306@gmail.com>
2023-03-01 15:38:11 +0100

    add base spinner

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+    return (
+        <>
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+        </>
+    )
+}
